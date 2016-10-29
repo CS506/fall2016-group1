3bfb0a74e54a82165a5fd14b082b4e82db1697d6
Zachary <zpreynol@iupui.edu>
2016-10-29 15:24:14 -0400

    Changed order of db fields

--- buckchat/app/models/User.js
+++ buckchat/app/models/User.js
@@ -1,10 +1,10 @@
 var mongodb = require('@onehilltech/blueprint-mongodb');
 
 var schema = new mongodb.Schema({
-  name: {type: String, lowercase: true, required: true, trim: true},
-  email: {type: String, lowercase: true, required: true, trim: true},
   username: {type: String, lowercase: true, required: true, trim: true},
   password: {type: String, required: true, trim: true},
+  name: {type: String, lowercase: true, required: true, trim: true},
+  email: {type: String, lowercase: true, required: true, trim: true}
 });
 
 const COLLECTION_NAME = 'user';
