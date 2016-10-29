6dedda5edf2a4951ad776f86e80f0fdb95477aae
Zachary <zpreynol@iupui.edu>
2016-10-29 15:20:18 -0400

    Removed serialize/deserialize functions

--- buckchat/app/listeners/app.init/initPassport.js
+++ buckchat/app/listeners/app.init/initPassport.js
@@ -11,7 +11,7 @@ function initPassport (app) {
   passport.use (new LocalStrategy (opts, authorize));
 
   function authorize (username, password, done) {
-    console.log("Authorizing user!!!!!!!!!!!!!!!!!!!!!");
+    console.log("Authorizing user!!!!!");
     User.findOne ({ username: username }, function (err, user) {
       if (err) { return done (err); }
       if (!user) { return done (null, false); }
@@ -19,22 +19,4 @@ function initPassport (app) {
       return done (null, user);
     });
   }
-
-
-  passport.serializeUser(function(user, done) {
-    /* convert user model to id */ 
-    console.log("serializing user (in init).........");
-    done(null, user.username);
-  });
-
-
-  passport.deserializeUser(function(id, done) {
-    /* convert id to a user model */
-    console.log("deserializing user (in init).........");
-    User.findOne({ username: id }, function (err, user) {
-      if (err) { return done(err); }
-      done(null, user);
-    });
-  });
-
 }
