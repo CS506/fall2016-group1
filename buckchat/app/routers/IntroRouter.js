1b721fc4177f7dad0474e55c26560e529885941f
Russ Sherman <sherman214@yahoo.com>
2016-10-30 22:07:59 -0400

    Revised IntroRouter to add register functions

--- buckchat/app/routers/IntroRouter.js
+++ buckchat/app/routers/IntroRouter.js
@@ -3,4 +3,10 @@ module.exports = exports = {
     get  : { view   : 'intro.pug' },
     post : { action : 'LoginController@login'},
   }
+
+  '/register' : {
+    get  : { view   : 'intro.pug' },
+    post : { action : 'RegisterController@register'},
+  }
+
 };
