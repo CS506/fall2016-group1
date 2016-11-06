4863da8484180e79bb942c9c09465e3ca1aae47d
raghav4494 <raghav4494@gmail.com>
2016-11-06 00:29:14 -0400

    Added Registration Functionality with Validation

--- buckchat/app/controllers/IntroPageController.js
+++ buckchat/app/controllers/IntroPageController.js
@@ -1,5 +1,6 @@
 var blueprint = require('@onehilltech/blueprint')
-  ;
+  , util      = require('util')
+  , User      = require ('../models/User');
 
 module.exports = IntroPageController;
 
@@ -33,12 +34,36 @@ IntroPageController.prototype.logout = function() {
     console.log('Logging user out.');
     req.logout();
     res.redirect('/login');
-  }
+  };
 };
 
 
 IntroPageController.prototype.register = function() {
   return function(req, res) {
-  // Nothing here yet
+
+    // Saving form details in local variables
+    var registerEmail = req.body.email;
+    var registerUsername = req.body.username;
+    var registerName = req.body.name;
+    var registerPassword = req.body.password;
+
+    //Checking for user exists already or not
+    User.findOne({ $or: [ { email: registerEmail }, { username: registerUsername }]},function(err,doc) {
+      if(err) {
+      return handleError(err);
+      }
+
+      //Indicating availability of username and email
+      if(doc === null) {  
+      //Assigning values to model variables  
+      var user1 = new User({name: registerName,email: registerEmail,username: registerUsername,password: registerPassword});
+      user1.save();
+      console.log('User successfully registered');
+      return res.render('welcome.pug');
+      }
+
+      //Indicating form values are present already
+      return res.render('intro.pug',{registerError: 'User exists already'});
+    });
   };
 };
--- buckchat/app/models/User.js
+++ buckchat/app/models/User.js
@@ -1,11 +1,13 @@
 var mongodb = require('@onehilltech/blueprint-mongodb');
 
 var schema = new mongodb.Schema({
-  username: {type: String, lowercase: true, required: true, trim: true},
-  password: {type: String, required: true, trim: true},
-  name: {type: String, lowercase: true, required: true, trim: true},
+  username: {type: String, lowercase: true, required: true, trim: true, minlength: 3, maxlength: 15},
+  password: {type: String, required: true, trim: true, minlength: 6, maxlength: 12},
+  name: {type: String, lowercase: true, required: true, trim: true, minlength: 1, maxlength: 30},
   email: {type: String, lowercase: true, required: true, trim: true}
 });
 
 const COLLECTION_NAME = 'user';
 module.exports = exports = mongodb.model(COLLECTION_NAME, schema);
+
+
