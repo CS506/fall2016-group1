d31badb195ff111bb6921d8b02c0c25c019fe1cf
Zachary <zpreynol@iupui.edu>
2016-11-25 21:47:11 -0500

    Made 'done' callback in helper functions optional

--- buckchat/tests/fixtures/TestHelper.js
+++ buckchat/tests/fixtures/TestHelper.js
@@ -8,17 +8,25 @@ var User = require('../../app/models/User.js')
  * Your test cases may call these functions before/after test cases as needed.
  */
 
-module.exports.insertUserDocs = function(done) {
+module.exports.insertUserDocs = function() {
+    args = arguments;
     // Insert a test user document in the database.
     var user = new User({name: 'joe', email: 'j@gmail.com', username: 'jjj', password: 'mypass'});
     user.save(function() {
-        done();
+        // If callback argument was supplied, invoke it.
+        if (args.length > 0) {
+            args[0]();
+        }
     });
 }
 
-module.exports.removeUserDocs = function(done) {
+module.exports.removeUserDocs = function() {
+    args = arguments;
     // Remove all test user documents in the database.
     User.find({ $or: [ { name: 'joe' }, { name: 'raghavendran' }]}).remove(function(err) {
-        done();
+        // If callback argument was supplied, invoke it.
+        if (args.length > 0) {
+            args[0]();
+        }
     });
 }
