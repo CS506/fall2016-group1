3615771d36bba1d6b54e46e4a85f501f48cf82e8
Zachary <zpreynol@iupui.edu>
2016-11-28 13:17:51 -0500

    Fixed 'name' lowercase bug - issue #23

--- buckchat/app/models/User.js
+++ buckchat/app/models/User.js
@@ -3,7 +3,7 @@ var mongodb = require('@onehilltech/blueprint-mongodb');
 var schema = new mongodb.Schema({
     username: {type: String, lowercase: true, required: true, trim: true, minlength: 3, maxlength: 15},
     password: {type: String, required: true, trim: true, minlength: 6, maxlength: 12},
-    name: {type: String, lowercase: true, required: true, trim: true, minlength: 1, maxlength: 30},
+    name: {type: String, required: true, trim: true, minlength: 1, maxlength: 30},
     email: {type: String, lowercase: true, required: true, trim: true}
 });
 
